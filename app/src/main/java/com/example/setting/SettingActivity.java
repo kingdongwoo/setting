96ada61870490ac1ce3e686ec34391b12c7756b3
ASUS <rlaehddn9403@naver.com>
2020-08-22 19:14:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/setting/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.setting;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.setting.R;
+
+public class SettingActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
